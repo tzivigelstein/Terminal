4473b2daf95df427771132885b076c2769a68e2a
Tzivi <stavgamer00@gmail.com>
2020-07-31 20:13:29 -0300

    Terminal V2 First commit

---
+++ article_switcher.js
@@ -0,0 +1,120 @@
+var time = new Date();
+var day = time.getUTCDate();
+var hours = time.getHours();
+var minutes = time.getMinutes();
+
+if (hours >= 01 && hours <= 02) {
+    document.getElementById("relatedA").style.display = "inherit";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 02 && hours <= 03) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "inherit";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 03 && hours <= 04) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "inherit";
+}
+if (hours >= 04 && hours <= 05) {
+    document.getElementById("relatedA").style.display = "inherit";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 05 && hours <= 06) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "inherit";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 06 && hours <= 7) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "inherit";
+}
+if (hours >= 7 && hours <= 8) {
+    document.getElementById("relatedA").style.display = "inherit";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 8 && hours <= 9) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "inherit";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 9 && hours <= 10) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "inherit";
+}
+if (hours >= 10 && hours <= 11) {
+    document.getElementById("relatedA").style.display = "inherit";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 11 && hours <= 12) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "inherit";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 12 && hours <= 13) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "inherit";
+}
+if (hours >= 13 && hours <= 14) {
+    document.getElementById("relatedA").style.display = "inherit";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 14 && hours <= 15) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "inherit";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 15 && hours <= 16) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "inherit";
+}
+if (hours >= 16 && hours <= 17) {
+    document.getElementById("relatedA").style.display = "inherit";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 17 && hours <= 18) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "inherit";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 18 && hours <= 19) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "inherit";
+}
+if (hours >= 19 && hours <= 20) {
+    document.getElementById("relatedA").style.display = "inherit";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 20 && hours <= 21) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "inherit";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 21 && hours <= 22) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "inherit";
+}
+if (hours >= 22 && hours <= 23) {
+    document.getElementById("relatedA").style.display = "inherit";
+    document.getElementById("relatedB").style.display = "none";
+    document.getElementById("relatedC").style.display = "none";
+}
+if (hours >= 23 && hours <= 24) {
+    document.getElementById("relatedA").style.display = "none";
+    document.getElementById("relatedB").style.display = "inherit";
+    document.getElementById("relatedC").style.display = "none";
+}
---
+++ main.js
@@ -0,0 +1,474 @@
+var a = 0;
+var b = 0;
+var time = new Date();
+var day = time.getUTCDate();
+var hours = time.getHours();
+var minutes = time.getMinutes();
+
+function show() {
+
+    if (hours == 01 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+    }
+
+    if (hours == 02 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 03 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 04 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 05 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 06 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 07 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 08 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 9 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+        b = b + 1
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+    }
+
+    if (hours == 10 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+        b = b + 1
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+    }
+
+    if (hours == 11 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 12 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 13 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+    }
+
+    if (hours == 14 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 15 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+    }
+
+    if (hours == 16 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+    }
+
+    if (hours == 17 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 18 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 19 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+    }
+
+    if (hours == 20 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 21 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 22 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+    }
+
+    if (hours == 23 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedC").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+
+    if (hours == 24 && minutes <= 59) {
+
+        if (b == 0) {
+            document.getElementById("relatedA").style.display = "inherit";
+        }
+
+
+        b = b + 1;
+
+        if (b == 2) {
+            document.getElementById("relatedB").style.display = "inherit";
+        }
+
+        if (b == 2) {
+            document.getElementById("bb").style.display = "none";
+            document.getElementById("cc").style.display = "inherit";
+        }
+
+    }
+}
+function less() {
+    if (a == 0) {
+        document.getElementById("relatedB").style.display = "none";
+        document.getElementById("relatedC").style.display = "none";
+        document.getElementById("bb").style.display = "inherit";
+        document.getElementById("cc").style.display = "none";
+    }
+    b = 0; a = 0;
+}
+
